7b8d2a95310511e1562ff1ce21b7a9f47a1f7f97
5844joan23 <100005844.joan23@fje.edu>
2020-11-30 20:47:49 +0100

    galeria y bbdd

--- js/code.js
+++ js/code.js
@@ -1,18 +1,19 @@
+window.onload=function() {
+    modal = document.getElementById('myModal');
+}
+
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    let modal = document.getElementById('myModal');
+    
     if (event.target == modal) {
-      modal.style.display = "none";
+    modal.style.display = "none";
     }
-  }
+}
 
 function openModal(){
-    let modal = document.getElementById('myModal');
     modal.style.display = "block";
 }
 
 function closeModal() {
-    // When the user clicks on <span> (x), close the modal
-    let modal = document.getElementById('myModal');
     modal.style.display = "none";
-}
+} 
